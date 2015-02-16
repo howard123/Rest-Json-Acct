836f1591fa84cc001a3381eb234fa84f8089eaa5
Howard <howards_s@yahoo.com>
2015-02-15 21:07:59 -0500

    add emailId.java class

---
+++ gs-rest-service/src/main/java/com/jack/obj/EmailId.java
@@ -0,0 +1,18 @@
+package com.jack.obj;
+
+public class EmailId {
+	
+	public EmailId(String emailId){
+		this.emailAddr = emailId;
+	}
+
+	public String getEmailAddr() {
+		return emailAddr;
+	}
+
+	public void setEmailAddr(String emailAddr) {
+		this.emailAddr = emailAddr;
+	}
+
+	public String emailAddr;
+}
