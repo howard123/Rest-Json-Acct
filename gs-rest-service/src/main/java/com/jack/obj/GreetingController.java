de0fadb35e207db7594ff36846f9c3f1b1dc0e9e
Howard <howards_s@yahoo.com>
2015-02-15 21:39:07 -0500

    add servlet

--- gs-rest-service/src/main/java/com/jack/obj/GreetingController.java
+++ gs-rest-service/src/main/java/com/jack/obj/GreetingController.java
@@ -3,25 +3,41 @@ package com.jack.obj;
 
 import hibernate.AccountFinding;
 import hibernate.Account;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
+    
+    
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    public ModelAndView  greeting(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+    	
+    	ModelAndView model = new ModelAndView("Greeting");
+        model.addObject("msg","Howard");
+    	
+        return model;
     }
+
+//    @RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(counter.incrementAndGet(),
+//                            String.format(template, name));
+//    }
     
 //    @RequestMapping("/account")
 //    public com.jack.obj.Account account(@RequestParam(value="emp_id", defaultValue= "2" ) long emp_num){
