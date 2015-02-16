7549be6826d20ff01736b5a063cd105eec0fe318
Howard <howards_s@yahoo.com>
2015-02-15 22:12:21 -0500

    add ResponseBody annotation to tell Spring that the return is not a view

--- gs-rest-service/src/main/java/com/jack/obj/GreetingController.java
+++ gs-rest-service/src/main/java/com/jack/obj/GreetingController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,7 +25,7 @@ public class GreetingController {
     
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public @ResponseBody Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
